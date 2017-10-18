ab9b5c2e02964677c3c0f0ca458dadbd47bb40db
Henry Wilson <m3henry@googlemail.com>
2017-10-18 01:11:43 +0100

    enumerated physical devices

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,15 @@
 #include <thread>
 #include <unistd.h>
 #include "glfw.hpp"
+#include <typeinfo>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 float color = 0.f;
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
-	//if(not glfwInit()) throw "glfwInit() failed";
+	std::cout << std::boolalpha << "Hello, world!\n" << std::endl;
+
     glfwWindowHint(GLFW_VISIBLE, false);
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
@@ -48,18 +49,55 @@ int main()
 			VK_API_VERSION_1_0
 		);
 
-		unsigned int glfwExtensionCount = 0;
+		unsigned int glfwExtensionCount;
+		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		std::cout << "Required glfwExtensions:\n";
+		for (unsigned int i = 0; i < glfwExtensionCount; ++i) std::cout << "    " << glfwExtensions[i] << '\n';
+		std::cout << std::endl;
 
 		vk::InstanceCreateInfo instanceCreateInfo(
 			vk::InstanceCreateFlags(),
 			&appInfo,
 			0, nullptr,
-			glfwExtensionCount, glfwGetRequiredInstanceExtensions(&glfwExtensionCount)
+			glfwExtensionCount, glfwExtensions
 		);
 
 	    auto instance = createInstance(instanceCreateInfo);
+
+		{
+			auto extension = vk::enumerateInstanceExtensionProperties();
+			std::cout << "Available Vulkan extensions:\n";
+			for (auto &x : extension) std::cout << "    " << x.extensionName << '\n';
+			std::cout << std::endl;
+		}
+
+		auto device = [&instance](){
+			auto devices = instance.enumeratePhysicalDevices();
+			std::cout << "Found " << devices.size() << " devices." << std::endl;
+			for (auto &d : devices)
+			{
+				auto properties = d.getProperties();
+				auto features = d.getFeatures();
+				std::cout << "    deviceType = " << to_string(properties.deviceType) << ",   geometryShader = " << (bool)features.geometryShader << std::endl;
+			}
+			std::cout << std::endl;
+			return devices.front();
+		}();
+
+		{
+			int graphicsFamily = -1;
+			auto queueProperties = device.getQueueFamilyProperties();
+			int i = 0;
+			for (auto &q : queueProperties)
+			{
+				if (q.queueCount > 0 && q.queueFlags & vk::QueueFlagBits::eGraphics) graphicsFamily = i;
+				if (graphicsFamily >= 0) break;
+				++i;
+			}
+			std::cout << "graphicsFamily = " << graphicsFamily << std::endl;
+		}
 	}
-	catch (std::runtime_error &exception)
+	catch (std::exception &exception)
 	{
 		std::cerr << exception.what() << std::endl;
 		return EXIT_FAILURE;
